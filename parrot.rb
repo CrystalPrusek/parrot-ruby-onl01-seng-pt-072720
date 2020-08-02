db6c9d7cbf377758d552bb5f22f57ae4f68bf2ac
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-02 16:06:31 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def restaurant
+  restaurant_name= "Guy's American Kitchen"
+  cuisine= "american"
+  motto= "Welcome to Flavor Town"
+end
